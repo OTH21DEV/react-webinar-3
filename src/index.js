86a9ea138188a469950118984e64582d3205b2c2
Oxana Theis <otheis@protonmail.com>
2023-05-29 22:37:53 +0100

    fix css for select element

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import {createRoot} from 'react-dom/client';
-import App from './app';
+import { createRoot } from "react-dom/client";
+import App from "./app";
 import Store from "./store";
-import {StoreContext} from "./store/context";
-import { LanguageProvider } from './containers/Language';
+import { StoreContext } from "./store/context";
+import { LanguageProvider } from "./containers/Language";
 
 const store = new Store();
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 // Первый рендер приложения
 root.render(
-  
   <LanguageProvider>
-  <StoreContext.Provider value={store}>
-    <App/>
-  </StoreContext.Provider>
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </LanguageProvider>
 );
