d2c7fcfb39dd7a42e791955f1e4be07a517f6020
Oxana Theis <otheis@protonmail.com>
2023-06-02 14:29:42 +0100

    add proptypes

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -1,6 +1,4 @@
-import { memo, useEffect,useCallback } from "react";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import { memo, useEffect, useCallback } from "react";
 import PageLayout from "../../components/page-layout";
 import Head from "../../components/head";
 import Navigation from "../../containers/navigation";
@@ -8,9 +6,12 @@ import useTranslate from "../../hooks/use-translate";
 import BtnLogin from "../../components/btn-login";
 import useSelector from "../../hooks/use-selector";
 import useStore from "../../hooks/use-store";
-import profileDetails from "../../components/profile-details";
 import ProfileDetails from "../../components/profile-details";
 
+/**
+ * Display user profile page
+ * @returns {HTMLElement}
+ */
 function Profile() {
   const tokenInLocalStorage = localStorage.getItem("token");
   const { t } = useTranslate();
@@ -28,18 +29,15 @@ function Profile() {
   }));
 
   const callbacks = {
+    //get token from API
     onSubmit: useCallback((login, password, navigate) => store.actions.login.getTokenFromApi(login, password, navigate), [store]),
-// onLogout:useCallback()=>
-
-onLogOut: useCallback((token) => store.actions.login.logOut(token), [store]),
-   
+    //delete user info on logout
+    onLogOut: useCallback((token) => store.actions.login.logOut(token), [store]),
   };
   return (
     <PageLayout>
-      <BtnLogin name={select.login.userName} toLogin={'/login'} toProfile={'/profile'} onLogOut={callbacks.onLogOut}/>
-      {/* <NavLogin name={select.login.userName} ></NavLogin> */}
+      <BtnLogin name={select.login.userName} toLogin={"/login"} toProfile={"/profile"} onLogOut={callbacks.onLogOut} />
       <Head title={t("title")} />
-
       <Navigation />
       <ProfileDetails profile={select.login} />
     </PageLayout>
--- src/components/profile-details/index.js
+++ src/components/profile-details/index.js
@@ -3,6 +3,13 @@ import PropTypes from "prop-types";
 import "./style.css";
 import { cn as bem } from "@bem-react/classname";
 
+/**
+ * Display user information
+ * @param {String} profile.userName user name 
+ * @param {String} profile.phone user phone 
+ * @param {String} profile.email user email
+ * @returns {HTMLElement}
+ */
 function ProfileDetails({ profile }) {
   const cn = bem("Profile");
   return (
@@ -26,5 +33,13 @@ function ProfileDetails({ profile }) {
     </div>
   );
 }
-
+ProfileDetails.propTypes = {
+    profile: PropTypes.objectOf(
+      PropTypes.shape({
+        userName: PropTypes.string,
+        phone: PropTypes.string,
+        email: PropTypes.string,
+      }).isRequired
+    ),
+  };
 export default memo(ProfileDetails);
